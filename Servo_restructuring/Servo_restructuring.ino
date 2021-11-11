f6f8ada55e25be7fa55233eee354371bb1d0432a
Ippyb <itzmeipsita@gmail.com>
2021-11-10 20:10:19 -0800

    Deleted Servo_restructuring (initialized version) and changed high/low values on Servo_Final

--- Servo_restructuring/Servo_restructuring.ino
+++ Servo_restructuring/Servo_restructuring.ino
@@ -18,7 +18,7 @@ class Valve {
       _servopin = servopin;
       _counter = counter;
       _servo = servo;
-      _buttonstate = LOW;     
+      _buttonstate = HIGH;     
     }
 };
 
@@ -26,6 +26,7 @@ class Valve {
     // star either means pointer or "give me the thing at the address"
 
     void setup() {
+      Serial.begin(9600);
   // put your setup code here, to run once:
       valves[0] = new Valve(8, 3, s1, 0);
       valves[1] = new Valve(9, 4, s2, 0);
@@ -47,25 +48,29 @@ class Valve {
       
       }
           
-      if (valves[0]->_buttonstate == LOW) {
+      if (valves[0]->_buttonstate == HIGH) {
         valves[0]->_servo.write(180); //zero degrees apparently(fact check pls)
       }
       else{
         valves[0]->_servo.write(20);
+        Serial.print("s1\n");
+
       }
     
-      if(valves[1]->_buttonstate == LOW) {
+      if(valves[1]->_buttonstate == HIGH) {
         valves[1]->_servo.write(20);
       }
       else{
         valves[1]->_servo.write(180);
+            Serial.print("s2\n");
       }
     
-      if(valves[2]->_buttonstate == LOW) {
+      if(valves[2]->_buttonstate == HIGH) {
         valves[2]->_servo.write(180);
       }
       else {
         valves[2]->_servo.write(20);
+            Serial.print("s3\n");
       }
       //else{
       //  valves[0]->_servo.write(20);
